ab0360dd0bc42a2f5a9d14c3c2bff9a14fe0c31f
explorer <explorer@explorer.explorer>
2023-05-23 09:51:57 +0300

    getusercmd

--- Entry_Point.cpp
+++ Entry_Point.cpp
@@ -229,12 +229,12 @@ __int32 __stdcall DllMain(void* This_Module_Location, unsigned __int32 Call_Reas
 				Redirection_Manager::Redirect_Function(Original_Weapon_Spread_Caller_Location, 1, (void*)605949248, 1, (void*)Redirected_Weapon_Spread);
 			}*/
 
-			/*_putws(L"[ + ] Input");
+			_putws(L"[ + ] Input");
 			{
-				Byte_Manager::Set_Bytes(1, (void*)604993824, 3, 144);
+				Byte_Manager::Set_Bytes(1, (void*)((unsigned __int32)Client_Module_Location + 1343395), 3, 144);
 
-				Redirection_Manager::Redirect_Function(Original_Copy_User_Command_Caller_Location, 0, (void*)604850464, 1, (void*)Redirected_Copy_User_Command);
-			}*/
+				//Redirection_Manager::Redirect_Function(Original_Copy_User_Command_Caller_Location, 0, (void*)604850464, 1, (void*)Redirected_Copy_User_Command);
+			}
 
 			_putws(L"[ + ] Network");
 			{

b36821ff5178ddb30d412ad7b741cbd98778bf9d
explorer <explorer@explorer.explorer>
2023-05-24 11:29:02 +0300

    Delete Shutdown.hpp

--- Shutdown.hpp
+++
@@ -1,6 +0,0 @@
-void* Original_Shutdown_Caller_Location;
-
-void __thiscall Redirected_Shutdown(void* Unknown_Parameter, char* Reason)
-{
-	(decltype(&Redirected_Shutdown)(Original_Shutdown_Caller_Location))(Unknown_Parameter, (char*)"Segregation");
-}

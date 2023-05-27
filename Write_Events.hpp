0aa5e61ee99f7c67efbc2c058bc26a28d4815b14
explorer <explorer@explorer.explorer>
2023-05-27 15:44:40 +0300

    Commit

--- Write_Events.hpp
+++ Write_Events.hpp
@@ -2,11 +2,14 @@ void* Original_Write_Events_Caller_Location;
 
 void __thiscall Redirected_Write_Events(void* Unknown_Parameter_1, void* Unknown_Parameter_2)
 {
-	Byte_Manager::Set_Bytes(0, Players_Data, sizeof(Players_Data), 0);
+	if (*(__int32*)((unsigned __int32)Engine_Module_Location + 4684008) == 5)
+	{
+		Byte_Manager::Set_Bytes(0, Players_Data, sizeof(Players_Data), 0);
 
-	Bruteforce_Reset_Tolerance();
+		Bruteforce_Reset_Tolerance();
 
-	Recent_Player_Data_Number = 0;
+		Recent_Player_Data_Number = 0;
+	}
 
 	(decltype(&Redirected_Write_Events)(Original_Write_Events_Caller_Location))(Unknown_Parameter_1, Unknown_Parameter_2);
 }

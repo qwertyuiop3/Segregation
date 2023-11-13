4b090684eaeb661a314c87a9fe7aae5630b58a24
explorer <explorer@explorer.explorer>
2023-11-13 09:40:18 +0300

    required for GetAbsVelocity call in RunPostThink during spread calculation

--- Finish_Move.hpp
+++ Finish_Move.hpp
@@ -12,4 +12,6 @@ void __thiscall Redirected_Finish_Move(void* Unknown_Parameter_1, void* Player, 
 	*(float*)((unsigned __int32)Move_Data + 76) = std::clamp(X, -90.f, 90.f);
 
 	(decltype(&Redirected_Finish_Move)(Original_Finish_Move_Caller))(Unknown_Parameter_1, Player, Unknown_Parameter_2, Move_Data);
+
+	*(__int32*)((unsigned __int32)Player + 372) |= 4096;
 }

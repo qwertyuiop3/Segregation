0178a63807d49f7a9705a933f22e0294ae0f9f2e
explorer <explorer@explorer.explorer>
2023-12-13 23:12:22 +0300

    .

--- Interpolate.hpp
+++ Interpolate.hpp
@@ -4,10 +4,7 @@ __int8 __thiscall Redirected_Interpolate(void* Entity, void* Unknown_Parameter)
 {
 	if (Entity != *(void**)((unsigned __int32)Client_Module + 5015784))
 	{
-		if (*(void**)Entity == (void*)((unsigned __int32)Client_Module + 3952404))
-		{
-			return 1;
-		}
+		return 1;
 	}
 
 	return (decltype(&Redirected_Interpolate)(Original_Interpolate_Caller))(Entity, Unknown_Parameter);

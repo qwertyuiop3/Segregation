70c177612ab9dcd6f153bdeec0bc56c33c42259e
explorer <explorer@explorer.explorer>
2023-12-28 01:17:11 +0300

    .

--- Update_Animation.hpp
+++ Update_Animation.hpp
@@ -4,7 +4,7 @@ void* Original_Update_Animation_Caller;
 
 void __thiscall Redirected_Update_Animation(void* Player)
 {
-	if (Player == *(void**)((unsigned __int32)Client_Module + 5015784))
+	if (Player == *(void**)607867332)
 	{
 		Byte_Manager::Copy_Bytes(0, (void*)((unsigned __int32)Player + 4124), sizeof(Update_Animation_Angles), Update_Animation_Angles);
 	}
